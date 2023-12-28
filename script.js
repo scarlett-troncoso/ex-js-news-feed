84e67fd154ec80f9584962b18b3b685ea8659958
scarlett-troncoso <scar.labussieri@gmail.com>
2023-12-28 15:31:10 +0100

    create array

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 console.log('funziona');
+
+const dati = [
+    {
+        id: 1 ,
+        title: 'Scoperta di una nuova specie di papera di gomma',
+        content: 'Scoperta di una nuova specie di papera di gomma.',
+        tags: 'geo, tech',
+        author: 'Diana Rossi',
+        published: '2023-02-11',
+        foto: 'https://picsum.photos/id/237/200/300'
+    },
+    {
+        id: 2,
+        title: 'Esplorando le profondità marine: il mistero degli abissi',
+        content: 'Esplorando le profondità marine: il mistero degli abissi',
+        tags: 'viaggi, geo',
+        author: 'Fabio Mari',
+        published: '2023-03-14',
+    },
+    {
+        id: 3,
+        title: 'Viaggio culinario: alla ricerca dei sapori perduti',
+        content: 'Esplorazione di tradizioni culinarie dimenticate e la ricerca di sapori autentici.' ,
+        tags: 'cucina',
+        author: 'Marta Bianchi',
+        published: '2023-04-20',
+    },
+    {
+        id: 4,
+        title: 'Arte moderna: oltre i confini convenzionali',
+        content: 'Un analisi delle tendenze e delle sfide nell\'arte contemporanea, con interviste a artisti emergenti.',
+        tags: 'arte, tech',
+        author: 'Gabriele Neri',
+        published: '2023-05-29',
+    }
+]
+
+console.log(dati);
+
